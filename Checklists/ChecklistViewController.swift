9f9e2ae0f1bf081443883c9c1d1a78f532441a06
Unknown <mlgrier@gmail.com>
2017-09-15 20:59:55 -0500

    updating the code

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var row0text = "Walk the dog"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Soccer practice"
+    var row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +34,7 @@ class ChecklistViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 100
+        return 5
         
     }
     
@@ -33,15 +45,15 @@ class ChecklistViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         
         if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
         } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
         } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
         } else if indexPath.row % 5 == 3 {
-            label.text = "Soccer practice"
+            label.text = row3text
         } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
+            label.text = row4text
         }
         
         return cell
