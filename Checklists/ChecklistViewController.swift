5dac947777adf7b90658a8c59a6b1a4da34d0087
Unknown <mlgrier@gmail.com>
2017-09-15 20:14:03 -0500

    adding code to checklist

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -47,6 +47,10 @@ class ChecklistViewController: UITableViewController {
         return cell
         
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
