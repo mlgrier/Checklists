6d73ade7a1edb43ee5e59a2d18a4dd21045855c9
Unknown <mlgrier@gmail.com>
2017-09-17 20:45:44 -0500

    added new checklistItem file

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -17,10 +17,10 @@ class ChecklistViewController: UITableViewController {
     var row4text = "Eat ice cream"
     
     var row0checked = false
-    var row1checked = false
-    var row2checked = false
+    var row1checked = true
+    var row2checked = true
     var row3checked = false
-    var row4checked = false
+    var row4checked = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,30 +66,20 @@ class ChecklistViewController: UITableViewController {
         
         if let cell = tableView.cellForRow(at: indexPath) {
             
-            var isChecked = false
-            
             if indexPath.row == 0 {
                 row0checked = !row0checked
-                isChecked = row0checked
             } else if indexPath.row == 1 {
                 row1checked = !row1checked
-                isChecked = row1checked
             } else if indexPath.row == 2 {
                 row2checked = !row2checked
-                isChecked = row2checked
             } else if indexPath.row == 3 {
                 row3checked = !row3checked
-                isChecked = row3checked
             } else if indexPath.row == 4 {
                 row4checked = !row4checked
-                isChecked = row4checked
             }
             
-            if isChecked {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
+            configureCheckmark(for: cell, at: indexPath)
+            
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
